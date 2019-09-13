da4a08823ac39e80494c9387c16bd04acb8db7f5
Tiffany K <tiffanykyi@gmail.com>
2019-09-13 14:32:49 -0700

    fix: Should evaluate environment variables by declaration order (#89)

--- test/index.test.js
+++ test/index.test.js
@@ -232,6 +232,7 @@ describe('config parser', () => {
             let namespaceTemplate;
             let defaultTemplate;
             let imagesTemplate;
+            let environmentTemplate;
             let restrictedjobTemplate;
 
             beforeEach(() => {
@@ -240,6 +241,7 @@ describe('config parser', () => {
                 namespaceTemplate = JSON.parse(loadData('templateWithNamespace.json'));
                 defaultTemplate = JSON.parse(loadData('templateWithDefaultNamespace.json'));
                 imagesTemplate = JSON.parse(loadData('templateWithImages.json'));
+                environmentTemplate = JSON.parse(loadData('templateWithEnvironment.json'));
                 restrictedjobTemplate = JSON.parse(loadData('templateWithRestrictedJobName.json'));
                 templateFactoryMock.getTemplate.withArgs('mytemplate@1.2.3')
                     .resolves(firstTemplate);
@@ -249,6 +251,8 @@ describe('config parser', () => {
                     .resolves(namespaceTemplate);
                 templateFactoryMock.getTemplate.withArgs('ImagesTestNamespace/imagestemplate@2')
                     .resolves(imagesTemplate);
+                templateFactoryMock.getTemplate.withArgs('mynamespace/environment@1.2.3')
+                    .resolves(environmentTemplate);
                 templateFactoryMock.getTemplate.withArgs('restrictedjob@1')
                     .resolves(restrictedjobTemplate);
             });
@@ -330,6 +334,34 @@ describe('config parser', () => {
                     .then(data => assert.deepEqual(
                         data, JSON.parse(loadData('basic-job-with-images.json'))
                     )));
+
+            it('flattens templates with environment variables properly',
+                () => parser(loadData('basic-job-with-environment.yaml'), templateFactoryMock)
+                    .then((data) => {
+                        /* eslint-disable */
+                        const expected = [{ BAR: 'foo' },
+                            { SD_TEMPLATE_FULLNAME: 'mynamespace/environment' },
+                            { SD_TEMPLATE_NAME: 'environment' },
+                            { SD_TEMPLATE_NAMESPACE: 'mynamespace' },
+                            { SD_TEMPLATE_VERSION: '1.2.3' },
+                            { BAZ: 'world' },
+                            { A: 'meow' },
+                            { B: '${A}' },
+                            { FOO: '${B}' }];
+                        const result = [];
+
+                        if (data) {
+                            // Create array of json objects
+                            for (var key in data.jobs.main[0].environment) {
+                                result.push({ [key]: data.jobs.main[0].environment[key] });
+                            }
+                            // Compare results
+                            for (var i in result) {
+                                assert.deepEqual(result[i], expected[i]);
+                            }
+                        }
+                        /* eslint-enable */
+                    }));
 
             it('returns error if template does not exist', () => {
                 templateFactoryMock.getTemplate.withArgs('mytemplate@1.2.3').resolves(null);
